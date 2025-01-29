f5fc6fc906decc7475b9d79e22ae69c3655410e5
Will Spies <willspies@Wills-MBP.fios-router.home>
2025-01-28 22:33:01 -0500

    Initial Crap Player

---
+++ godot-learning-project/src/player/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+var direction: Vector2 = Vector2.ZERO
+var speed: int = 300
+
+func _physics_process(delta: float) -> void:
+	gravity(delta)
+	direction = Input.get_vector("Left", "Right", "Dummy", "Dummy")
+	if Input.is_action_just_pressed("Jump"):
+		jump()
+	velocity = speed * direction
+	move_and_slide()
+	
+	
+func gravity(delta: float) -> void:
+	if self.is_on_floor():
+		velocity.y = 0
+	else:
+		velocity.y += ProjectSettings.get_setting("physics/2d/default_gravity")
+	move_and_slide()
+
+func jump() -> void:
+	velocity.y = -10000
+	move_and_slide()
+	
